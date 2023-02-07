df14f69100d562b9658280126ff5ef86e5db2db5
Reed <reed@plusreed.com>
2023-02-07 13:24:01 -0500

    add deprecated text

--- src/utils/twitter.ts
+++ src/utils/twitter.ts
@@ -11,6 +11,13 @@ const client = new Twitter({
     access_token_secret:    (process.env.TW_ACCESS_SECRET as string)
 })
 
+/**
+ * @deprecated Due to the new Twitter API rules and pricing, this is no longer maintained.
+ * 
+ * @description Posts a Tweet to Twitter.
+ * @param content The content of the tweet.
+ * @returns void. Returns nothing.
+ */
 async function tweet(content: string): Promise<void> {
     if (content.length > 280) throw new Error('Status is longer than 280 characters.')
 
